7baca9da17a2f729fab97f8198092bb8a226df7b
Timothé Coniel <timothe@coniel.com>
2023-03-30 03:05:06 +0200

    Add: New route for Vercel testing

--- GasPrice/backend/dist/routes/defaultRoute.js
+++ GasPrice/backend/dist/routes/defaultRoute.js
@@ -4,6 +4,9 @@ exports.defaultRoute = void 0;
 const express_1 = require("express");
 exports.defaultRoute = (0, express_1.Router)();
 exports.defaultRoute.get("/", (req, res) => {
-    res.send("coucou emilie");
+    res.send("Server up to date");
+});
+exports.defaultRoute.get("/try", (req, res) => {
+    res.send("Route for testing stuff");
 });
 //# sourceMappingURL=defaultRoute.js.map
--- GasPrice/backend/src/routes/defaultRoute.ts
+++ GasPrice/backend/src/routes/defaultRoute.ts
@@ -3,5 +3,9 @@ import { Router } from "express";
 export const defaultRoute = Router();
 
 defaultRoute.get("/", (req, res) => {
-  res.send("coucou emilie");
+  res.send("Server up to date");
+});
+
+defaultRoute.get("/try", (req, res) => {
+  res.send("Route for testing stuff");
 });
