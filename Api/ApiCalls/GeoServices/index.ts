b48f031443e8c9fddb6e23bdeaf9b5e192d8809d
titi0267 <timothe@coniel.com>
2024-01-25 19:08:26 +0800

    Fix: Coords truncated to fetch dep codes

--- Api/ApiCalls/GeoServices/index.ts
+++ Api/ApiCalls/GeoServices/index.ts
@@ -3,8 +3,11 @@ import { GeoReverse } from "../../constants/geoReverse";
 import makeRequest from "../../services/request.services";
 
 const getCodeFromCoords = async (coords: [number, number]): Promise<string> => {
+  console.log(coords);
   const res = await makeRequest<{ features: Array<GeoCodingList> }>(
-    `https://api-adresse.data.gouv.fr/reverse?lon=${coords[0].toString()}&lat=${coords[1].toString()}&limit=1`,
+    `https://api-adresse.data.gouv.fr/reverse?lon=${coords[0]
+      .toFixed(2)
+      .toString()}&lat=${coords[1].toFixed(2).toString()}&limit=1`,
     "GET",
     "GeoServices",
   );
--- dist/ApiCalls/GeoServices/index.js
+++ dist/ApiCalls/GeoServices/index.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const request_services_1 = __importDefault(require("../../services/request.services"));
 const getCodeFromCoords = async (coords) => {
-    const res = await (0, request_services_1.default)(`https://api-adresse.data.gouv.fr/reverse?lon=${coords[0].toString()}&lat=${coords[1].toString()}&limit=1`, "GET", "GeoServices");
+    console.log(coords);
+    const res = await (0, request_services_1.default)(`https://api-adresse.data.gouv.fr/reverse?lon=${coords[0]
+        .toFixed(2)
+        .toString()}&lat=${coords[1].toFixed(2).toString()}&limit=1`, "GET", "GeoServices");
     let departement_code = "01";
     if (res && res.features.length == 1) {
         departement_code = res.features[0].properties.id.substring(0, 2);
