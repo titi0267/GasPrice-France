b82c15a9d367aaf4209e22164ca6bf58dc438cb7
titi0267 <timothe@coniel.com>
2024-02-13 02:40:26 +0800

    Fix: Inverted coords in osrm request

--- Api/ApiCalls/Osrm/index.ts
+++ Api/ApiCalls/Osrm/index.ts
@@ -5,11 +5,7 @@ import { OsrmType } from "../../Types";
 
 const generateGeoJsonCoordinates = async (start: string, end: string) => {
   const generationResult = await makeRequest<OsrmType>(
-    `http://router.project-osrm.org/route/v1/driving/${
-      start.split(",")[1] + "," + start.split(",")[0]
-    };${
-      end.split(",")[1] + "," + end.split(",")[0]
-    }?alternatives=2&geometries=geojson&overview=full`,
+    `http://router.project-osrm.org/route/v1/driving/${start};${end}?alternatives=2&geometries=geojson&overview=full`,
     "GET",
     "osrm",
   );
--- dist/ApiCalls/Osrm/index.js
+++ dist/ApiCalls/Osrm/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateGeoJsonCoordinates = void 0;
 const request_services_1 = __importDefault(require("../../services/request.services"));
 const generateGeoJsonCoordinates = async (start, end) => {
-    const generationResult = await (0, request_services_1.default)(`http://router.project-osrm.org/route/v1/driving/${start.split(",")[1] + "," + start.split(",")[0]};${end.split(",")[1] + "," + end.split(",")[0]}?alternatives=2&geometries=geojson&overview=full`, "GET", "osrm");
+    const generationResult = await (0, request_services_1.default)(`http://router.project-osrm.org/route/v1/driving/${start};${end}?alternatives=2&geometries=geojson&overview=full`, "GET", "osrm");
     generationResult.routes.forEach(route => {
         route.type = "Feature";
         delete route?.legs;
