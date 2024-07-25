a969971264d274f4155eaefa836b9b718724f542
Sylvian BURN <sylvian.burn@epitech.eu>
2024-07-25 20:02:46 +0900

    fix: add cors support for web version

--- Api/index.ts
+++ Api/index.ts
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 import ENV from "./env";
 
 const app = express();
+const cors = require('cors');
+
+
+//cors
+app.use(cors());
+
 app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
